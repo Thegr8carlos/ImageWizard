91700626ecc114c9a50f119124305e8e8624608f
Haniel Ulises <hanielulises2003@gmail.com>
2023-11-18 21:05:01 -0600

    Added basic filters functions definition

---
+++ Image_tool/Filters/basicFilters.cpp
@@ -0,0 +1,1 @@
+#include "basicFilters.h"
---
+++ Image_tool/Filters/basicFilters.h
@@ -0,0 +1,9 @@
+// To do
+unsigned char* toGrayScale ();
+unsigned char* binarize ();
+unsigned char* guassianBlur ();
+unsigned char* medianFilter ();
+unsigned char* sobelFilter ();
+unsigned char* laplacianFilter ();
+unsigned char* lowPassFilter ();
+unsigned char* highPassFilter ();
